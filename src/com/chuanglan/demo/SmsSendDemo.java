7c367f800a2d1f46945a29a25e08f201bedcf24a
tianyongh <yhonghong@foxmail.com>
2017-06-13 14:17:28 +0800

    Create SmsSendDemo.java

---
+++ src/com/chuanglan/demo/SmsSendDemo.java
@@ -0,0 +1,52 @@
+package com.chuanglan.demo;
+
+import java.io.UnsupportedEncodingException;
+import com.alibaba.fastjson.JSON;
+import com.chuanglan.sms.request.SmsSendRequest;
+import com.chuanglan.sms.response.SmsSendResponse;
+import com.chuanglan.sms.util.ChuangLanSmsUtil;
+/**
+ * 
+ * @author tianyh 
+ * @Description:普通短信发送
+ */
+public class SmsSendDemo {
+
+	public static final String charset = "utf-8";
+	// 用户平台API账号(非登录账号,示例:N1234567)
+	public static String account = "";
+	// 用户平台API密码(非登录密码)
+	public static String pswd = "Dasibugaosuni12";
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+
+		//请求地址请登录253云通讯自助通平台查看或者询问您的商务负责人获取
+		String smsSingleRequestServerUrl = "http://xxx/msg/send/json";
+		// 短信内容
+	    String msg = "【253云通讯】你好,你的验证码是123456";
+		//手机号码
+		String phone = "18721605772";
+		//状态报告
+		String report= "true";
+		
+		SmsSendRequest smsSingleRequest = new SmsSendRequest(account, pswd, msg, phone);
+		
+		smsSingleRequest.setReport(report);
+		
+		String requestJson = JSON.toJSONString(smsSingleRequest);
+		
+		System.out.println("before request string is: " + requestJson);
+		
+		String response = ChuangLanSmsUtil.sendSmsByPost(smsSingleRequestServerUrl, requestJson);
+		
+		System.out.println("response after request result is :" + response);
+		
+		SmsSendResponse smsSingleResponse = JSON.parseObject(response, SmsSendResponse.class);
+		
+		System.out.println("response  toString is :" + smsSingleResponse);
+		
+	
+	}
+
+
+}
