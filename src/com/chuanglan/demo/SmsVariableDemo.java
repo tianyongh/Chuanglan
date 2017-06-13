71c8be954f11c464bd50faccebdea73cf83712e2
tianyongh <yhonghong@foxmail.com>
2017-06-13 14:19:42 +0800

    Create SmsVariableDemo.java

--- src/com/chuanglan/demo/SmsVariableDemo.java
+++ src/com/chuanglan/demo/SmsVariableDemo.java
@@ -23,41 +23,26 @@ public class SmsVariableDemo {
 		//请求地址请登录253云通讯自助通平台查看或者询问您的商务负责人获取
 		String smsVariableRequestUrl = "http://xxx/msg/variable/json";
 		//短信内容
-		String msg = "【253云通讯】尊敬的{$var},您好,您的验证码是{$var},{$var}
-
-分钟内有效";
-		//参数组									
-
-							
-		String params = "187********,女士,123456,3;130********,先
-
-生,123456,3;";
+		String msg = "【253云通讯】尊敬的{$var},您好,您的验证码是{$var},{$var}分钟内有效";
+		
+		//参数组								
+		String params = "187********,女士,123456,3;130********,先生,123456,3;";
 		//状态报告
 		String report= "true";
 		
-		SmsVariableRequest smsVariableRequest=new SmsVariableRequest
-
-(account, pswd, msg, params, report);
+		SmsVariableRequest smsVariableRequest=new SmsVariableRequest(account, pswd, msg, params, report);
 		
-        String requestJson = JSON.toJSONString(smsVariableRequest);
+       		String requestJson = JSON.toJSONString(smsVariableRequest);
 		
 		System.out.println("before request string is: " + requestJson);
 		
-		String response = ChuangLanSmsUtil.sendSmsByPost
-
-(smsVariableRequestUrl, requestJson);
+		String response = ChuangLanSmsUtil.sendSmsByPost(smsVariableRequestUrl, requestJson);
 		
-		System.out.println("response after request result is : " + 
-
-response);
+		System.out.println("response after request result is : " + response);
 		
-		SmsVariableResponse smsVariableResponse = JSON.parseObject(response, 
-
-SmsVariableResponse.class);
+		SmsVariableResponse smsVariableResponse = JSON.parseObject(response, SmsVariableResponse.class);
 		
-		System.out.println("response  toString is : " + 
-
-smsVariableResponse);
+		System.out.println("response  toString is : " + smsVariableResponse);
 		
 	}
 
